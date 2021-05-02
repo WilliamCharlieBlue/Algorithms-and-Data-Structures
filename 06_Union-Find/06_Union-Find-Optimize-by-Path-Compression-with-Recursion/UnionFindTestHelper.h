9cc936a85688ea8221704b388668624a0e15dd41
pydata <pydata@2ndedition.com>
2021-05-03 01:03:27 +0800

    Optimize by Path Compression Recursion (excellent in theory but it depends)

---
+++ 06_Union-Find/06_Union-Find-Optimize-by-Path-Compression-with-Recursion/UnionFindTestHelper.h
@@ -0,0 +1,137 @@
+#ifndef UNION_FIND_TEST_HELPER_H
+#define UNION_FIND_TEST_HELPER_H
+
+#include <iostream>
+#include <ctime>
+#include "UnionFind1.h"
+#include "UnionFind2.h"
+#include "UnionFind3.h"
+#include "UnionFind4.h"
+#include "UnionFind5.h"
+#include "UnionFind6.h"
+
+using namespace std;
+
+namespace UnionFindTestHelper{
+    void testUF1( int n ){
+        srand( time(NULL) );
+        UF1::UnionFind uf = UF1::UnionFind(n);
+        time_t startTime = clock();
+
+        for( int i = 0 ; i < n ; i ++ ){
+            int a = rand()%n;
+            int b = rand()%n;
+            uf.unionElements(a,b);
+        }
+        for(int i = 0 ; i < n ; i ++ ){
+            int a = rand()%n;
+            int b = rand()%n;
+            uf.isConnected(a,b);
+        }
+        time_t endTime = clock();
+        cout<<"Quick Find"<<endl;
+        cout<<"UF1, "<<2*n<<" ops, "<<double(endTime-startTime)/CLOCKS_PER_SEC<<" s"<<endl;
+    }
+
+    void testUF2( int n ){
+        srand( time(NULL) );
+        UF2::UnionFind uf = UF2::UnionFind(n);
+        time_t startTime = clock();
+
+        for( int i = 0 ; i < n ; i ++ ){
+            int a = rand()%n;
+            int b = rand()%n;
+            uf.unionElements(a,b);
+        }
+        for(int i = 0 ; i < n ; i ++ ){
+            int a = rand()%n;
+            int b = rand()%n;
+            uf.isConnected(a,b);
+        }
+        time_t endTime = clock();
+        cout<<"Quick Union"<<endl;
+        cout<<"UF2, "<<2*n<<" ops, "<<double(endTime-startTime)/CLOCKS_PER_SEC<<" s"<<endl;
+    }
+
+    void testUF3( int n ){
+        srand( time(NULL) );
+        UF3::UnionFind uf = UF3::UnionFind(n);
+        time_t startTime = clock();
+
+        for( int i = 0 ; i < n ; i ++ ){
+            int a = rand()%n;
+            int b = rand()%n;
+            uf.unionElements(a,b);
+        }
+        for(int i = 0 ; i < n ; i ++ ){
+            int a = rand()%n;
+            int b = rand()%n;
+            uf.isConnected(a,b);
+        }
+        time_t endTime = clock();
+        cout<<"Optimize by Size"<<endl;
+        cout<<"UF3, "<<2*n<<" ops, "<<double(endTime-startTime)/CLOCKS_PER_SEC<<" s"<<endl;
+    }
+
+    void testUF4( int n ){
+        srand( time(NULL) );
+        UF4::UnionFind uf = UF4::UnionFind(n);
+        time_t startTime = clock();
+
+        for( int i = 0 ; i < n ; i ++ ){
+            int a = rand()%n;
+            int b = rand()%n;
+            uf.unionElements(a,b);
+        }
+        for(int i = 0 ; i < n ; i ++ ){
+            int a = rand()%n;
+            int b = rand()%n;
+            uf.isConnected(a,b);
+        }
+        time_t endTime = clock();
+        cout<<"Optimize by Rank"<<endl;
+        cout<<"UF4, "<<2*n<<" ops, "<<double(endTime-startTime)/CLOCKS_PER_SEC<<" s"<<endl;
+    }
+
+    void testUF5( int n ){
+        srand( time(NULL) );
+        UF5::UnionFind uf = UF5::UnionFind(n);
+        time_t startTime = clock();
+
+        for( int i = 0 ; i < n ; i ++ ){
+            int a = rand()%n;
+            int b = rand()%n;
+            uf.unionElements(a,b);
+        }
+        for(int i = 0 ; i < n ; i ++ ){
+            int a = rand()%n;
+            int b = rand()%n;
+            uf.isConnected(a,b);
+        }
+        time_t endTime = clock();
+        cout<<"Optimize by Path Compression"<<endl;
+        cout<<"UF5, "<<2*n<<" ops, "<<double(endTime-startTime)/CLOCKS_PER_SEC<<" s"<<endl;
+    }
+
+    void testUF6( int n ){
+        srand( time(NULL) );
+        UF6::UnionFind uf = UF6::UnionFind(n);
+        time_t startTime = clock();
+
+        for( int i = 0 ; i < n ; i ++ ){
+            int a = rand()%n;
+            int b = rand()%n;
+            uf.unionElements(a,b);
+        }
+        for(int i = 0 ; i < n ; i ++ ){
+            int a = rand()%n;
+            int b = rand()%n;
+            uf.isConnected(a,b);
+        }
+        time_t endTime = clock();
+        cout<<"Optimize by Path Compression Recursion"<<endl;
+        cout<<"UF6, "<<2*n<<" ops, "<<double(endTime-startTime)/CLOCKS_PER_SEC<<" s"<<endl;
+    }
+}
+
+#endif
---
+++ 06_Union-Find/06_Union-Find-Optimize-by-Path-Compression-with-Recursion/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "UnionFindTestHelper.h"
+
+using namespace std;
+
+int main() {
+    int n = 10000;
+
+    UnionFindTestHelper::testUF1(n);
+    cout<<endl;
+    n = 100000;
+    UnionFindTestHelper::testUF1(n);
+    cout<<endl;
+    UnionFindTestHelper::testUF2(n);
+    cout<<endl;
+    UnionFindTestHelper::testUF3(n);
+    cout<<endl;
+    UnionFindTestHelper::testUF4(n);
+    cout<<endl;
+    UnionFindTestHelper::testUF5(n);
+    cout<<endl;
+    UnionFindTestHelper::testUF6(n);
+    cout<<endl;
+}
