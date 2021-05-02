7e956c14a7417c4d7293b463059865eab26c6459
pydata <pydata@2ndedition.com>
2021-05-03 00:59:07 +0800

    speed up union using parent-Root theory

---
+++ 06_Union-Find/02_Qiuck-Union/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "UnionFindTestHelper.h"
+
+using namespace std;
+
+int main() {
+    int n = 10000;
+
+    UnionFindTestHelper::testUF1(n);
+    cout << endl;
+    n = 100000;
+    UnionFindTestHelper::testUF1(n);
+    cout << endl;
+    UnionFindTestHelper::testUF2(n);
+    cout << endl;
+}
