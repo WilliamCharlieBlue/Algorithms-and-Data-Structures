374eb348f35b86151427dfdfd7cfbd3207602202
pydata <pydata@2ndedition.com>
2021-05-05 14:51:01 +0800

    comparison of LazyPrim with Prim

--- 08_Mininum-Span-Trees/03_Prim/main.cpp
+++ 08_Mininum-Span-Trees/03_Prim/main.cpp
@@ -11,6 +11,87 @@
 
 using namespace std;
 
+int main_performance(){
+    string filename3 = "testG3.txt";
+    int V3 = 8;
+    string filename4 = "testG4.txt";
+    int V4 = 250;
+    string filename5 = "testG5.txt";
+    int V5 = 1000;
+    string filename6 = "testG6.txt";
+    int V6 = 10000;
+
+    // 文件读取
+    SparseGraph<double> g3 = SparseGraph<double>(V3, false);
+    ReadGraph<SparseGraph<double>, double> readGraph3(g3, filename3);
+    cout<<filename3<<" load successfully."<<endl;
+
+    SparseGraph<double> g4 = SparseGraph<double>(V4, false);
+    ReadGraph<SparseGraph<double>, double> readGraph4(g4, filename4);
+    cout<<filename4<<" load successfully."<<endl;
+
+    SparseGraph<double> g5 = SparseGraph<double>(V5, false);
+    ReadGraph<SparseGraph<double>, double> readGraph5(g5, filename5);
+    cout<<filename5<<" load successfully."<<endl;
+
+    SparseGraph<double> g6 = SparseGraph<double>(V6, false);
+    ReadGraph<SparseGraph<double>, double> readGraph6(g6, filename6);
+    cout<<filename6<<" load successfully."<<endl;
+
+    cout<<endl;
+
+    clock_t startTime, endTime;
+
+    // Test Lazy Prim MST
+    cout<<"Test Lazy Prim MST: "<<endl;
+
+    startTime = clock();
+    LazyPrimMST<SparseGraph<double>, double> lazyPrimMst3(g3);
+    endTime = clock();
+    cout<<"TEST for G3: "<<(double )(endTime-startTime)/CLOCKS_PER_SEC<<" s."<<endl;
+
+    startTime = clock();
+    LazyPrimMST<SparseGraph<double>, double> lazyPrimMst4(g4);
+    endTime = clock();
+    cout<<"TEST for G4: "<<(double )(endTime-startTime)/CLOCKS_PER_SEC<<" s."<<endl;
+
+    startTime = clock();
+    LazyPrimMST<SparseGraph<double>, double> lazyPrimMst5(g5);
+    endTime = clock();
+    cout<<"TEST for G5: "<<(double )(endTime-startTime)/CLOCKS_PER_SEC<<" s."<<endl;
+
+    startTime = clock();
+    LazyPrimMST<SparseGraph<double>, double> lazyPrimMst6(g6);
+    endTime = clock();
+    cout<<"TEST for G6: "<<(double )(endTime-startTime)/CLOCKS_PER_SEC<<" s."<<endl;
+
+    cout<<endl;
+
+    // Test Prim MST
+    cout<<"Test Prim MST: "<<endl;
+
+    startTime = clock();
+    PrimMST<SparseGraph<double>, double> PrimMst3(g3);
+    endTime = clock();
+    cout<<"TEST for G3: "<<(double )(endTime-startTime)/CLOCKS_PER_SEC<<" s."<<endl;
+
+    startTime = clock();
+    PrimMST<SparseGraph<double>, double> PrimMst4(g4);
+    endTime = clock();
+    cout<<"TEST for G4: "<<(double )(endTime-startTime)/CLOCKS_PER_SEC<<" s."<<endl;
+
+    startTime = clock();
+    PrimMST<SparseGraph<double>, double> PrimMst5(g5);
+    endTime = clock();
+    cout<<"TEST for G5: "<<(double )(endTime-startTime)/CLOCKS_PER_SEC<<" s."<<endl;
+
+    startTime = clock();
+    PrimMST<SparseGraph<double>, double> PrimMst6(g6);
+    endTime = clock();
+    cout<<"TEST for G6: "<<(double )(endTime-startTime)/CLOCKS_PER_SEC<<" s."<<endl;
+
+}
+
 int main() {
 
     // testG3.txt
@@ -45,6 +126,10 @@ int main() {
         cout << mst[i] << endl;
     cout << "The MST weight is: " << PrimMst.result() << endl;
     cout << endl;
+
+    // LazyPrim 和 Prim 性能比较
+    main_performance();
+
     return 0;
 
 }
